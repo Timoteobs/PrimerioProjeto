598294f68cf4edc07104def44c92d2905728a4d4
timoteoSilveira <timoteo.barros@gmail.com>
2023-09-22 15:29:59 -0300

    feat: create method calculate

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -22,12 +22,9 @@ namespace PrimeiroProjeto {
             triangleY.SideB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             triangleY.SideC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double p = (triangleX.SideA + triangleX.SideB + triangleX.SideC) / 2.0;
-            double areaX = Math.Sqrt(p * (p - triangleX.SideA) * (p - triangleX.SideB) * (p - triangleX.SideC));
 
-            p = (triangleY.SideA + triangleY.SideB + triangleY.SideC) / 2.0;
-            double areaY = Math.Sqrt(p * (p - triangleY.SideA) * (p - triangleY.SideB) * (p - triangleY.SideC));
-
+            double areaX = triangleX.CalculateArea();
+            double areaY = triangleY.CalculateArea();
 
             Console.WriteLine("Area de x = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
             Console.WriteLine("Area de y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
--- PrimeiroProjeto/Triangle.cs
+++ PrimeiroProjeto/Triangle.cs
@@ -9,5 +9,12 @@ namespace PrimeiroProjeto {
         public double SideA;
         public double SideB;
         public double SideC;
+
+        public double CalculateArea() {
+            double p = (SideA + SideB + SideC) / 2.0;
+            double root = Math.Sqrt(p * (p - SideA) * (p - SideB) * (p - SideC));
+
+            return root;
+        }
     }
 }
